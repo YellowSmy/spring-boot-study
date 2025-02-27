4e6137b43ee088c808620ca147b3a2aa67130c65
YellowSmy <fprh2005@naver.com>
2025-02-27 20:13:11 +0900

    Repository Interface create - Junit depend. add

--- src/main/java/com/example/sbb/question/Question.java
+++ src/main/java/com/example/sbb/question/Question.java
@@ -1,30 +1,38 @@
-package com.example.sbb;
+package com.example.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.example.sbb.answer.Answer;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter //일반적으로는 권장 X
+@Setter //일반적으로는 권장 X / 분리
 @Entity
-public class Answer {
+public class Question {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 200)
+	private String subject;
 	
 	@Column(columnDefinition = "TEXT")
 	private String content;
 	
 	private LocalDateTime createDate;
 	
-	@ManyToOne
-	private Question question;	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
+	
 }
